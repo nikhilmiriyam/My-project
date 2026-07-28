c28545415bb5ee839152dc1bd4dfd644306698d2
nikhilmiriyam <m9925410@gmail.com>
2026-07-28 11:52:39 +0530

    Disable cache for auth pages

--- server/server.js
+++ server/server.js
@@ -12,31 +12,49 @@ const HOST = process.env.HOST || '0.0.0.0';
 const APP_NAME = process.env.APP_NAME || 'RideShare';
 const clientDir = path.join(__dirname, '..', 'client');
 
+function setNoCacheHeaders(res) {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+}
+
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  if (req.path.match(/\.(html|js|css)$/)) {
+    setNoCacheHeaders(res);
+  }
+  next();
+});
 app.use(express.static(clientDir));
 
 app.get('/', (req, res) => {
+  setNoCacheHeaders(res);
   res.sendFile(path.join(clientDir, 'index.html'));
 });
 
 app.get('/dashboard.html', (req, res) => {
+  setNoCacheHeaders(res);
   res.sendFile(path.join(clientDir, 'dashboard.html'));
 });
 
 app.get('/account.html', (req, res) => {
+  setNoCacheHeaders(res);
   res.sendFile(path.join(clientDir, 'account.html'));
 });
 
 app.get('/history.html', (req, res) => {
+  setNoCacheHeaders(res);
   res.sendFile(path.join(clientDir, 'history.html'));
 });
 
 app.get('/signin.html', (req, res) => {
+  setNoCacheHeaders(res);
   res.sendFile(path.join(clientDir, 'signin.html'));
 });
 
 app.get('/signup.html', (req, res) => {
+  setNoCacheHeaders(res);
   res.sendFile(path.join(clientDir, 'signup.html'));
 });
 
